c34442bc81e845fc6284b51be934dbe7207ec2cf
Mohammad Ali Nateqy <mohammadalinateqy@gmail.com>
2023-05-12 18:40:54 +0430

    Dice Aplication

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(
+  return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          title: Center(
+            child: Text('Dicee'),
+          ),
           backgroundColor: Colors.red,
-          title: Text('Dice'),
         ),
-        body: MyApp(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void diceNumber(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                diceNumber();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                diceNumber();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
